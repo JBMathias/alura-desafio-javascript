41f1651443eb9cfe948bd66cafe3868ffaaae465
João Matias <joaojr.musica@gmail.com>
2022-04-03 15:00:14 -0300

    #7DaysOfCode Dia 2: Obtendo Informações do Usuário *Uso de Template literals *Uso de JavaScript no HTML *Informações Obtidas do Usário no Navegador

--- desafio.js
+++ desafio.js
@@ -1,28 +1,23 @@
 /* #7DaysOfCode
 
-    Dia 1: Operador de comparação
+    Dia 2: Obtendo Informações do Usuário
 
 */
+const nome = prompt("Qual o seu nome? ")
+const idade = prompt("Quantos anos você tem? ")
+const linguagem = prompt("Qual linguagem de programação você está estudando? ")
+const mensagem = `Olá ${nome}, você tem ${idade} e já está aprendendo ${linguagem}!`
+alert(mensagem)
+const resposta = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÂO.`)
+document.write(`<h3>Nome: ${nome}<br>Idade: ${idade} Anos<br>Linguagem: ${linguagem}<br><br></h3>`)
+document.write(`<h3>Pergunta: Você gosta de estudar ${linguagem}?</h3>`)
 
-let numeroUm =1
-let stringUm = '1'
-let numeroTrinta = 30
-let stringTrinta = '30'
-let numeroDez = 10
-let stringDez = '10'
-
-if(numeroUm == stringUm){
-    console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes\n')
+if(resposta == 1){
+    document.write(`<h3>Resposta: SIM <br></h3>`)
+    document.write("<h3>> Muito bom! Continue estudando e você terá muito sucesso.</h3>")
+}else if(resposta == 2){
+    document.write(`<h3>Resposta: NÃO<br></h3>`)
+    document.write("<h3>> Ahh que pena... Já tentou aprender outras linguagens?</h3>")
 }else{
-    console.log('As variáveis numeroUm e stringUm não tem o mesmo valor\n')
-}
-if(numeroTrinta === stringTrinta){
-    console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo\n')
-}else{
-    console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo\n')
-}
-if(numeroDez == stringDez){
-    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes\n')
-}else{
-    console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+    document.write(`<h3>> Você digitou ${resposta}. Por Favor, Digite apenas 1 ou 2.</h3>`)
 }
